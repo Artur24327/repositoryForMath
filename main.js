64b1416012550428f74ba00ad48704ef336bc7d8
Artur24327 <57686461+Artur24327@users.noreply.github.com>
2021-03-09 21:43:04 +0200

    Laba

---
+++ main.js
@@ -0,0 +1,15 @@
+const min = require('min').min;
+const abs = require('abs').abs;
+const summ = require('summ').summ;
+
+let array1=[23, 324, 5436, 4];
+
+min(array1);
+
+console.log('');
+
+summ(23, 324, 5436, 4);
+
+console.log('');
+
+abs(-32);
